c8c57e21e7f2202918053980d9fb6032d360ec71
FIRST_NAME LAST_NAME <MY_NAME@example.com>
2023-01-01 10:09:10 +0530

    change method from onclick to eventListener

--- index.js
+++ index.js
@@ -1,38 +1,42 @@
 let homeScore = 0
 let guestScore = 0
-let homeEl = document.getElementById("home-score")
-let guestEl = document.getElementById("guest-score")
+const homeEl = document.getElementById("home-score")
+const guestEl = document.getElementById("guest-score")
+
+const addOneHome = document.getElementById("home-add-one")
+const addTwoHome = document.getElementById("home-add-two")
+const addThreeHome = document.getElementById("home-add-three")
+
+const addOneGuest = document.getElementById("guest-add-one")
+const addTwoGuest = document.getElementById("guest-add-two")
+const addThreeGuest = document.getElementById("guest-add-three")
+
 homeEl.textContent = homeScore
 guestEl.textContent = guestScore
+
 //HOME
-function addOneHome()
-{
+addOneHome.addEventListener("click",()=>{
     homeScore += 1
-    homeEl.textContent = homeScore        
-}
-function addTwoHome()
-{
+    homeEl.textContent = homeScore 
+})
+addTwoHome.addEventListener("click",()=>{
     homeScore += 2
-    homeEl.textContent = homeScore        
-}
-function addThreeHome()
-{
+    homeEl.textContent = homeScore 
+})
+addThreeHome.addEventListener("click",()=>{
     homeScore += 3
-    homeEl.textContent = homeScore        
-}
+    homeEl.textContent = homeScore 
+})
 // GUEST
-function addOneGuest()
-{
+addOneGuest.addEventListener("click",()=>{
     guestScore += 1
-    guestEl.textContent = guestScore        
-}
-function addTwoGuest()
-{
+    guestEl.textContent = guestScore 
+})
+addTwoGuest.addEventListener("click",()=>{
     guestScore += 2
-    guestEl.textContent = guestScore        
-}
-function addThreeGuest()
-{
+    guestEl.textContent = guestScore  
+})
+addThreeGuest.addEventListener("click",()=>{
     guestScore += 3
-    guestEl.textContent = guestScore        
-}
+    guestEl.textContent = guestScore  
+})
